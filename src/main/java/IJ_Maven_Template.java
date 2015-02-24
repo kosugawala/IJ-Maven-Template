1e363a5d718561d5c2f8efdebd78c8f8473ea4e3
Ko Sugawara <939.dogra.magra@gmail.com>
2015-02-24 12:47:01 +0900

    first commit

---
+++ src/main/java/IJ_Maven_Template.java
@@ -0,0 +1,16 @@
+import ij.IJ;
+import ij.plugin.PlugIn;
+
+/**
+ * IJ Maven Template
+ *
+ * @author Ko Sugawara
+ */
+public class IJ_Maven_Template implements PlugIn {
+
+	@Override
+	public void run(String arg) {
+		IJ.log("Hello World!");
+	}
+
+}
